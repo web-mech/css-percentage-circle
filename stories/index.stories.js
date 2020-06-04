84805b5a4cd49eb9088cb1808f9e2d2900ff56d7
webmech <webmech@gmail.com>
2020-06-04 13:17:53 -0400

    refactor(*): refactor default sizes and placement
    updates for sizing to be more realistic. not making the widget huge by default. also removes
    floating and margins by default
    
    BREAKING CHANGE: changes to the naming. added huge, large and medium sizes. placement by default is
    neutral

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -7,6 +7,15 @@ export default {
   title: 'CSS Percentage Circles',
 }
 
+export const Micro = () => `<div class="c100 p25 micro">
+      <span>25%</span>
+      <div class="slice">
+          <div class="bar"></div>
+          <div class="fill"></div>
+      </div>
+  </div>
+`
+
 export const Small = () => `<div class="c100 p25 small">
       <span>25%</span>
       <div class="slice">
@@ -16,7 +25,16 @@ export const Small = () => `<div class="c100 p25 small">
   </div>
 `
 
-export const Medium = () => `<div class="c100 p50 green">
+export const Medium = () => `<div class="c100 p25 medium">
+      <span>25%</span>
+      <div class="slice">
+          <div class="bar"></div>
+          <div class="fill"></div>
+      </div>
+  </div>
+`
+
+export const Large = () => `<div class="c100 p50 green large">
       <span>50%</span>
       <div class="slice">
           <div class="bar"></div>
@@ -25,7 +43,7 @@ export const Medium = () => `<div class="c100 p50 green">
   </div>
 `
 
-export const Large = () => `<div class="c100 p75 big orange">
+export const Huge = () => `<div class="c100 p75 huge orange">
       <span>75%</span>
       <div class="slice">
           <div class="bar"></div>
